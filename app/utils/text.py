9338a17a01ece28cd7a3ead25b6b2f51127935be
N3gretenok <160652304+N3gretenok@users.noreply.github.com>
2024-04-18 20:47:47 +0500

    Add files via upload

---
+++ app/utils/text.py
@@ -0,0 +1,8 @@
+text_data = {
+    'start': 'start',
+    'choosing_class': 'Выберите раздел',
+    'catalog': 'Выберите предмет',
+    'output_elements': 'Выберите интересующую вас тему',
+    'send_data': 'send_data',
+    'information': 'information'
+}
